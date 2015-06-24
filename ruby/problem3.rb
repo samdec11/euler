5307dc789564f78a02429f817d126b0c80a992c0
Sean Marzug-McCarthy <samdec11@gmail.com>
2015-06-24 23:26:30 +0200

    bleh

--- ruby/problem3.rb
+++ ruby/problem3.rb
@@ -4,35 +4,16 @@
 # What is the largest prime factor of the number 600851475143?
 
 def run
-
-end
-# require 'mathn'
-#
-# def run(num)
-#   x = (num / 2.0).to_i
-#   found = false
-#   while found == false
-#     if x == 1
-#       break
-#     elsif num % x == 0
-#       if x.prime?
-#         found = true
-#       else
-#         x -= 1
-#       end
-#     else
-#       x -= 1
-#     end
-#   end
-#   x
-# end
-
-def is_prime?(num)
-  if num == 0
-    false
-  elsif num == 1 || num == 2
-    true
-  else
-    (2..Math.sqrt(num).floor).detect{|i| num % i == 0}.nil?
+  n = 600851475143
+  factor = 3
+  while n > 1
+    if n % factor == 0
+      n = n/factor
+    else
+      factor += 2
+    end
   end
+  factor
 end
+
+# 6857
